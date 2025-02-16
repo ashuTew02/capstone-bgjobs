1982391f1918b7c748dd86299c757aa995d3daae
Ashutosh Tewari <ashutoshtewari41@gmail.com>
2025-02-16 19:00:48 +0530

    initial commit

---
+++ src/main/java/com/capstone/bgjobs/BgjobsApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.bgjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgjobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/bgjobs/BgjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.bgjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
